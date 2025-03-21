f8e66908609b9595cbdd40b5f12f4da1cabbaa98
Teo Stei <teodora.stei@gmail.com>
2025-03-21 16:41:11 +0100

    Kept only the Python script and results folder

--- MindWanderingProject/initial.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt
-
-print("NumPy version:", np.__version__)
-print("Matplotlib version:", plt.matplotlib.__version__)
--- MindWanderingProject/open.py
+++
@@ -1,6 +0,0 @@
-file_path = "/Users/teostei/Desktop/MINDWANDERINGDEEPLEARNING/data_theodora/Scirpts/BVxEyetrack.py"
-
-with open(file_path, "r") as file:
-    content = file.read()
-
-print(content)  # Display file content
